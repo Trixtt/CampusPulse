dbd8e530b3331815245d5e4846064f821bd1b24f
Trixt <muhammadtriosaputra123@gmail.com>
2026-05-26 07:29:39 +0700

    second commit

--- database/migrations/2026_05_24_131823_create_report_updates_table.php
+++ database/migrations/2026_05_24_131823_create_report_updates_table.php
@@ -6,20 +6,21 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
-        Schema::create('report_updates', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+        if (!Schema::hasTable('report_updates')) {
+
+            Schema::create('report_updates', function (Blueprint $table) {
+
+                $table->id();
+
+                $table->timestamps();
+
+            });
+
+        }
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('report_updates');
